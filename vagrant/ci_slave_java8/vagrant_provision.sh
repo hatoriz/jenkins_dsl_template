ab526e73d088b2cfb8876f5964f5f7e6407d2fbc
hatoriz <ninja.hatoriz@gmail.com>
2017-05-21 18:16:40 +0700

    [VAGRANT] - Initiate CI Slave, Java 8 and Gradle

---
+++ vagrant/ci_slave_java8/vagrant_provision.sh
@@ -0,0 +1,23 @@
+"Updating apt-get repo"
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+
+echo "Installing Git.."
+sudo apt-get install -y git
+
+echo "Installing Java 8.."
+sudo apt-get install -y software-properties-common python-software-properties
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#sudo add-apt-repository ppa:webupd8team/java -y
+#sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+echo "Setting environment variables for Java 8..."
+sudo apt-get install -y oracle-java8-set-default
+
+echo "Installing Gradle 3.5"
+#sudo add-apt-repository ppa:cwchien/gradle
+#sudo apt-get update
+sudo apt-get install gradle -y
